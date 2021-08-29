bae7bdf4d52e0459362d4e53c6e474c67799fb33
MOUKAILA <yahaya.moukaila@gmail.com>
2021-08-29 17:24:26 -0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] tab = {"mange", "orange", "goyave", "cerise", "papaye"};
+
+        caracteres(tab);
+        System.out.println(fact(5));
+    }
+
+    public static void caracteres(String[] tab) {
+        for(String fruit: tab) {
+            if(fruit.equals("orange")) {
+                System.out.print("1 - ");
+            } else {
+                for (int i = 0; i < fruit.length(); i++ ) {
+                   System.out.print(fruit.charAt(i) + " - ");
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    public static long fact(int n) {
+        if(n < 1) {
+            return 1;
+        } else {
+            return n * fact(n - 1);
+        }
+    }
+}
